8ca8382c299668c6ee0c64db0bda4fb1d38eb055
Lamina Samuel <samuellamina2@gmail.com>
2021-11-01 18:44:32 +0100

    delete task &  toggle reminders

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: "Docs appiontment",
+      text: "Docs appointment",
       day: "Feb 5th at 2:30pm",
       reminder: true,
     },
@@ -25,10 +25,28 @@ function App() {
     },
   ]);
 
+  // delete task ...
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to Show"
+      )}
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,17 @@
 import Tasks from "./Tasks";
+import CancelIcon from "@mui/icons-material/Cancel";
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
-      <h3>{task.text}</h3>
-      <h3>{task.day}</h3>
+    <div className="task" onDoubleClick={() => onToggle(task.id)}>
+      <h3>
+        {task.text}{" "}
+        <CancelIcon
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
     </div>
   );
 };
